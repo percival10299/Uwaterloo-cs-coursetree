c6456eafb1ae9a668d0ebfb643642fac6e2fe66f
Haron Wang <Haoran0425@gmail.com>
2026-02-10 22:27:44 -0500

    PGSQL

--- uw-course-tree/src/App.js
+++ uw-course-tree/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import CytoscapeComponent from "react-cytoscapejs";
 import cytoscape from "cytoscape";
 import dagre from "cytoscape-dagre";
